d20b847053e11e6f96a9c5ddad0b3e5e20d0189b
Derril87 <yaroslav.balyuk@gmail.com>
2025-08-19 16:45:32 -0400

    add: add solvation to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const arr = [];
+  const pair = {};
+
+  bracketsConfig.forEach(([start, finish]) => {
+    pair[start] = finish;
+  });
+
+  for (let i = 0; i < str.length; i += 1) {
+    const item = str[i];
+    if (item in pair) {
+      if (pair[item] === item && arr[arr.length - 1] === item) {
+        arr.pop();
+      } else {
+        arr.push(item);
+      }
+    } else {
+      const last = arr.pop();
+      if (pair[last] !== item) return false;
+    }
+  }
+  return arr.length === 0;
 };
